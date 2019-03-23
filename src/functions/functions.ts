e0cf676148fc0855a1d9abf7e771c7d61aa60112
Adam Krantz <Adam.Krantz@microsoft.com>
2019-03-23 15:01:18 -0700

    function to write numbers as words

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,116 +1,136 @@
-﻿/**
- * Adds two numbers.
- * @customfunction 
- * @param first First number
- * @param second Second number
- * @returns The sum of the two numbers.
- */
-function add(first: number, second: number): number {
-  return first + second;
-}
-CustomFunctions.associate("ADD", add);
+﻿const wordsForNumbers = [
+  "Zero",
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Eleven",
+  "Twelve",
+  "Thirteen",
+  "Fourteen",
+  "Fifteen",
+  "Sixteen",
+  "Seventeen",
+  "Eighteen",
+  "Nineteen"
+];
+const wordsForTens = [
+  "Zero",
+  "Ten",
+  "Twenty",
+  "Thirty",
+  "Forty",
+  "Fifty",
+  "Sixty",
+  "Seventy",
+  "Eighty",
+  "Ninety",
+];
+const wordForHundred = "Hundred";
+const wordsForPowersOfThousand = [
+  "",
+  "Thousand",
+  "Million",
+  "Billion",
+  "Trillion",
+  "Quadrillion",
+  "Quintillion",
+  "Sextillion",
+  "Septillion",
+  "Octillion",
+  "Nonillion",
+  "Decillion"
+];
 
 /**
- * Displays the current time once a second.
- * @customfunction 
- * @param invocation Custom function handler  
- */
-function clock(invocation: CustomFunctions.StreamingInvocation<string>): void {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-CustomFunctions.associate("CLOCK", clock);
-
-/**
- * Computes the nth Fibonacci number.
- * @customfunction FIBONACCI 斐波那契
+ * Write out a number using English words.
+ * @customfunction
  * @param n number
  */
-function fib(n: number): number {
-  if (n < 0) {
-    throw new Error("n cannot be negative.");
+function numberToWords(n: number): string {
+  let words: string;
+
+  if (n === 0) {
+    words = numberToWords(n);
+  } else {
+    let current = n;
+    let wordGroups = [];
+    let power = 0;
+
+    while (current > 0) {
+      const hundreds = current % 1000;
+      let wordGroup = "";
+
+      if (hundreds > 0) {
+        wordGroup = `${hundredsToWords(hundreds)}`;
+
+        if (wordGroups.length > 0) {
+          if (wordsForPowersOfThousand[power] === undefined) throw new Error("Number is too large.");
+          wordGroup += ` ${wordsForPowersOfThousand[power]} `;
+        }
+      }
+
+      wordGroups.push(wordGroup);
+      power += 1;
+      current = Math.floor(current / 1000);
+    }
+
+    words = wordGroups.reverse().join("").trim();
   }
 
-  const values: number[] = [1, 1];
+  return words;
+}
+CustomFunctions.associate("NUMBERTOWORDS", numberToWords);
 
-  for (let index = 2; index <= n; ++index) {
-    values[index % 2] = values[0] + values[1];
+function lessThanTwentyToWord(n: number): string {
+  if (n >= 20) throw new Error("The number should be less than 20.");
+  if (n < 0) throw new Error("The number should not be negative.");
+  return wordsForNumbers[n];
+}
+
+function hundredsToWords(n: number): string {
+  if (n >= 1000) throw new Error("The number should be less than 1000.");
+  if (n < 0) throw new Error("The number should not be negative.");
+  let words = "";
+
+  if (n === 0) {
+    words = wordsForNumbers[n];
+  } else {
+    let current = n;
+    const hundreds = Math.floor(current / 100);
+    current %= 100;
+    const isHundredMultiple = current === 0;
+    const tens = Math.floor(current / 10);
+    current %= 10;
+    const ones = current;
+
+    if (hundreds > 0) {
+      words += `${wordsForNumbers[hundreds]} ${wordForHundred}`;
+
+      // add a space unless it is a multiple of 100
+      if (!isHundredMultiple) {
+        words += " ";
+      }
+    }
+
+    if (tens < 2) {
+      if (!isHundredMultiple) {
+        words += lessThanTwentyToWord(tens * 10 + ones);
+      }
+    } else {
+      words += `${wordsForTens[tens]}`;
+
+      if (ones !== 0) {
+        words += `-${wordsForNumbers[ones]}`;
+      }
+    }
   }
 
-  return values[n % 2];
+  return words;
 }
-CustomFunctions.associate("FIBONACCI", fib);
-
-
-let count: number = 0;
-
-/**
- * Returns the number of times that the function was called
- * @customfunction
- */
-function callCount(): number {
-  ++count;
-
-  return count;
-}
-CustomFunctions.associate("callCount", callCount);
-
-/**
- * Returns the current time.
- * @customfunction 
- * @returns String with the current time formatted for the current locale.
- */
-function currentTime(): string {
-  return new Date().toLocaleTimeString();
-}
-
-/**
- * Increments a value once a second.
- * @customfunction 
- * @param incrementBy Amount to increment
- * @param invocation Custom function handler 
- */
-function increment(incrementBy: number, invocation: CustomFunctions.StreamingInvocation<number>): void {
-  let result = 0;
-  const timer = setInterval(() => {
-    result += incrementBy;
-    invocation.setResult(result);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-CustomFunctions.associate("INCREMENT", increment);
-
-/**
- * Writes a message to console.log().
- * @customfunction 
- * @param message String to write.
- * @returns String to write.
- */
-function logMessage(message: string): string {
-  console.log(message);
-
-  return message;
-}
-CustomFunctions.associate("LOG", logMessage);
-
-/**
- * Regular expression test
- * @customfunction
- * @param string The string to test
- * @param pattern Regular expression pattern
- * @returns True if it matches the pattern; false otherwise
- */
-function regex(string: string, pattern: string): boolean {
-  return new RegExp(pattern).test(string);
-}
-CustomFunctions.associate("REGEX", regex);
-
